604bf4843cfa3169f0963bad391761d5aff91863
Shivang-Soni <soni.shivang.999@gmail.com>
2025-12-03 21:50:15 +0530

    Update config.py: Add persistent directory variable

--- backend/core/config.py
+++ backend/core/config.py
@@ -7,6 +7,9 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "UHPM-Agent"
     ENV: str = os.getenv("ENV", "dev")
+    PERSIST_DIRECTORY: str = os.getenv(
+        "PERSIST_DIRECTORY", "backend/chroma_store"
+        )
 
 
 settings = Settings()
