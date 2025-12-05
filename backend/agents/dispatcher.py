87c1aea31316d8694015bef98daf34a001685fea
Shivang-Soni <soni.shivang.999@gmail.com>
2025-12-05 08:19:31 +0530

    Update dispatcher.py: add user input functionality

--- backend/agents/dispatcher.py
+++ backend/agents/dispatcher.py
@@ -2,6 +2,7 @@ import logging
 
 from agents.research_agent import ResearchAgent
 
+# Logger setup
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.info)
 
@@ -13,7 +14,13 @@ class Dispatcher:
         # self.content_agent = ContentAgent()
         # self.experiment_agent = ExperimentAgent()
     
-    def run(self, reason_output: dict):
+    def run(self, reason_output: dict, user_payload: dict):
+        """
+        Orchestrates actual execution of the selected agent
+
+        -reason_output: dict: The output from the Reasoning Agent
+        -user_payload: dict: the user input payload from the API call
+        """
         task_type = reason_output.get("task_type")
         action = reason_output.get("action")
         inputs_needed = reason_output.get("inputs_needed", {})
@@ -21,11 +28,29 @@ class Dispatcher:
         logger.info(
             f"Dispatcher received task: {task_type} with action: {action}"
             )
+        
+        # Check if required inputs are present already
+        missing_inputs = [
+            field for field in inputs_needed
+            if field not in user_payload
+        ]
+
+        if missing_inputs:
+            return {
+                "status": "waiting_for_inputs",
+                "missing": missing_inputs,
+                "what_is_needed": inputs_needed
+            }
 
         if task_type == "research":
+            result = self.research_agent.analyse_product(
+                product_text=user_payload.get("product_text", ""),
+                competitor_text=user_payload.get("competitor_text", "")
+            )
+
             return {
-                "status": "running_research_agent",
-                "expected_inputs": inputs_needed
+                "status": "research_completed",
+                "agent_result": result
             }
         # fallback for unrecognised task type(s)
         return {
