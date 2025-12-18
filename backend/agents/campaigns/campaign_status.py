28add40b24b85c729ea23589c7ec53504918602f
Shivang-Soni <soni.shivang.999@gmail.com>
2025-12-18 13:04:16 +0530

    Update campaign_status.py

--- backend/agents/campaigns/campaign_status.py
+++ backend/agents/campaigns/campaign_status.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class CampaignStatus(str, Enum):
     CREATED = "created"
+    PREVIEWING = "previewing"
     PREVIEWED = "previewed"
     PUBLISHING = "publishing"
     PUBLISHED = "published"
