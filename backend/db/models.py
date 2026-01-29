400911f994030071f330b9365796e3479dfd8fb9
Shivang-Soni <soni.shivang.999@gmail.com>
2026-01-29 12:59:08 +0530

    Update models.py

---
+++ backend/db/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, JSON, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+import uuid
+
+from db.session import Base
+
+
+class Campaign(Base):
+    __tablename__ = "campaigns"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    name = Column(String, nullable=True)
+
+    state = Column(String, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), onupdate=func.now()
+    )
